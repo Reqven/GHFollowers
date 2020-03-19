f4d1a35f98cce2b866a5ad40b01844ae0088a158
Reqven <manuu.marchand@gmail.com>
2020-03-19 20:19:32 +0100

    implementing loading indicator

--- GHFollowers/Screens/FollowersVC.swift
+++ GHFollowers/Screens/FollowersVC.swift
@@ -48,8 +48,10 @@ class FollowersVC: UIViewController {
     }
     
     func getFollowers(page: Int) {
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             
             switch(result) {
                 case .success(let followers):
