78354b0e06bf1ab036a062f9a540336c00568d3f
Manu Marchand <manuel.marchand@saint-gobain.com>
2020-03-19 12:46:43 +0100

    fix project files

--- GHFollowers/Extensions/UIViewController+Ext.swift
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -12,7 +12,7 @@ extension UIViewController {
     
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
-            let alertVC = GFAlert(title: title, message: message, buttonTitle: buttonTitle)
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true)
